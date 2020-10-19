a88f88489b6186f3928fd43a3aa9de83281e27e0
Henrique Fittipaldi <henrique.gfitti@gmail.com>
2020-10-19 16:14:36 -0300

    buscando_dados

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,13 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = 'Aparecida Nutricionista';
+
+
+var paciente = document.querySelector('#primeiro-paciente');
+
+var tdPeso = paciente.querySelector('.info-peso');
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector('.info-altura');
+var altura = paciente.textContent;
+
+var imc = peso / (altura * altura);
