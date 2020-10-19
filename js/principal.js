2824c241b633436e4f1a97f0159f303e7eede5ae
Henrique Fittipaldi <henrique.gfitti@gmail.com>
2020-10-19 16:21:08 -0300

    exibindo_imc

--- js/principal.js
+++ js/principal.js
@@ -8,6 +8,8 @@ var tdPeso = paciente.querySelector('.info-peso');
 var peso = tdPeso.textContent;
 
 var tdAltura = paciente.querySelector('.info-altura');
-var altura = paciente.textContent;
+var altura = tdAltura.textContent;
 
+var tdImc = paciente.querySelector('.info-imc');
 var imc = peso / (altura * altura);
+tdImc.textContent = imc;
